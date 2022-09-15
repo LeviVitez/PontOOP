73dc9d1c2244e2ae31500a9e4ecedabe4d19c0b6
Bóta László Levente <bota.laszlo.levente@petrik.hu>
2022-09-15 08:52:13 +0200

    pont osztaly

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -1,7 +1,12 @@
 package hu.petrik.pontoop;
 
+import hu.petrik.pontoop.pont.Pont;
+
 public class Main {
     public static void main(String[] args) {
-
+        Pont p1=new Pont();
+        Pont p2=new Pont(0,0);
+        Pont p3=new Pont(3,4);
+        System.out.println(p1);
     }
 }
--- src/hu/petrik/pontoop/pont/Pont.java
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -1,15 +1,15 @@
 package hu.petrik.pontoop.pont;
 
-public class pont {
+public class Pont {
     private int x;
     private int y;
 
-    public pont(){
+    public Pont(){
         x=0;
         y=0;
     }
 
-    public pont(int x,int y){
+    public Pont(int x,int y){
         this.x=x;
         this.y=y;
     }
