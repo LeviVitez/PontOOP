db7b87f95e5d32d40729b7d417c055ac46fe9f94
Bóta László Levente <bota.laszlo.levente@petrik.hu>
2022-09-15 08:45:51 +0200

    pont osztaly

--- src/hu/petrik/pontoop/pont/pont.java
+++ src/hu/petrik/pontoop/pont/pont.java
@@ -1,4 +1,16 @@
 package hu.petrik.pontoop.pont;
 
 public class pont {
+    private int x;
+    private int y;
+
+    public pont(){
+        x=0;
+        y=0;
+    }
+
+    public pont(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
 }
